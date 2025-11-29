720d2d0ddfc55157a5444840ecdd61ee33f973c5
Kiran Panchavati <kiranpanchavati9@gmail.com>
2025-11-29 20:34:17 +0530

    Files has been updated

--- mongodb.sh
+++ mongodb.sh
@@ -1,23 +1,9 @@
-#!/bin/bash
-if [ "$EUID" -ne 0 ]; then
-  sudo bash "$0" "$@"
-  exit
-fi
-
-echo "Installing MongoDB..."
-
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 
-rpm --import https://www.mongodb.org/static/pgp/server-7.0.asc
+dnf install mongodb-org -y
 
-dnf clean all
-dnf makecache
-dnf install -y mongodb-org mongodb-org-server
-
-sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongodb.conf
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
 systemctl enable mongod
-systemctl restart mongod
 
-echo "MongoDB installed successfully!"
-systemctl status mongod --no-pager
+systemctl start mongod
