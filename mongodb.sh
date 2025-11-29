500b0360c8262f4ad85916c44e392dcb3651f055
Kiran Panchavati <kiranpanchavati9@gmail.com>
2025-11-29 20:25:21 +0530

    Updated mongodb.sh file

--- mongodb.sh
+++ mongodb.sh
@@ -1,8 +1,21 @@
+#!/bin/bash
+
+echo "Installing MongoDB..."
+
+# Copy repo file
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 
-dnf install mongodb-org -y
+# Install MongoDB
+dnf clean all
+dnf makecache
+dnf install -y mongodb-org
 
+# Update bind IP
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
+# Enable + Start service
 systemctl enable mongod
 systemctl restart mongod
+
+# Status Check
+systemctl status mongod
