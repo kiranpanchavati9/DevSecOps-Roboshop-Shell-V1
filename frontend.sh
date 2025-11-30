4a0e4c941047ffe24c5c40973c49e4b7dd5e9822
Kiran Panchavati <kiranpanchavati9@gmail.com>
2025-11-30 08:15:17 +0530

    frontend nginx.conf file updated

--- frontend.sh
+++ frontend.sh
@@ -9,7 +9,7 @@ curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip
 
-cp nginx.conf /etc/nginx/nginx.conf 
+sudo cp nginx.conf /etc/nginx/nginx.conf
 
 systemctl enable nginx 
 systemctl restart nginx 
