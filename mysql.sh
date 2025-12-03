daa815e4d60d6732e21396f964dff471fd2b6aa7
Kiran Panchavati <kiranpanchavati9@gmail.com>
2025-12-03 08:15:22 +0530

    mysql.sh has been updated

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,19 @@
+#dnf install mysql-server -y
+
+#systemctl enable mysqld
+#systemctl restart mysqld
+
+#mysql_secure_installation --set-root-pass RoboShop@1
+
+#!/bin/bash
+
 dnf install mysql-server -y
 
 systemctl enable mysqld
-systemctl restart mysqld  
+systemctl restart mysqld
 
-mysql_secure_installation --set-root-pass RoboShop@1
+# Setting root password (EL9 compatible)
+mysql --connect-expired-password -uroot <<EOF
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'RoboShop@1';
+FLUSH PRIVILEGES;
+EOF
